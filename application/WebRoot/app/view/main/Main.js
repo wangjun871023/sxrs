48fd89a03d27a5555fa730b3db102a1130d8eef6
Administrator <Administrator@AX-wangjun-PC.AX.CLIC>
2014-11-13 15:55:54 +0800

    f

--- application/WebRoot/app/view/main/Main.js
+++ application/WebRoot/app/view/main/Main.js
@@ -2,120 +2,43 @@ Ext.define('app.view.main.Main', {
 	extend : 'Ext.container.Container',
 	requires : [ 'app.view.main.MainController', 'app.view.main.region.Top',
 			'app.view.main.region.Bottom', 'app.view.main.login.Login',
-			'app.view.main.MainModel' ],
+			'app.view.main.region.Left', 'app.view.main.region.Right',
+			'app.view.main.region.Center', 'app.view.main.MainModel' ],
 	xtype : 'app-main',
 	controller : 'main',
+	autoScroll : true,
 	viewModel : {
 		type : 'main'
 	},
 	layout : {
-		type : 'border'
+		type : 'hbox'
 	},
 	items : [ {
-		region : 'center',
-		autoScroll : true,
+		xtype:"panel",
+		flex:1
+	},{
+		flex:4,
 		items : [ {
 			xtype : 'maintop',
-			region : 'north',
-			height : 80
+			height : 100
 		}, {
-			xtype : "mainlogin",
-			title : "登录"
-		}, {
-			xtype : "panel",
-			title : "站内导航",
-			layout : {
-				type : "table",
-				columns : 2
-			},
+			layout : "column",
 			items : [ {
-				xtype : "button",
-				text : "办公室"
+				columnWidth : 0.2,
+				xtype : "mainleft"
 			}, {
-				xtype : "button",
-				text : "工会工作部"
+				columnWidth : 0.5,
+				xtype : "maincenter"
 			}, {
-				xtype : "button",
-				text : "监察部"
-			}, {
-				xtype : "button",
-				text : "销售督察部"
-			}, {
-				xtype : "button",
-				text : "内控合规部"
-			}, {
-				xtype : "button",
-				text : "人力资源部"
-			}, {
-				xtype : "button",
-				text : "教育培训部"
-			}, {
-				xtype : "button",
-				text : "电销中心"
-			}, {
-				xtype : "button",
-				text : "健康保险部"
-			}, {
-				xtype : "button",
-				text : "团体业务部"
-			}, {
-				xtype : "button",
-				text : "个险销售部"
-			}, {
-				xtype : "button",
-				text : "县域保险部"
-			}, {
-				xtype : "button",
-				text : "银行保险部"
-			}, {
-				xtype : "button",
-				text : "客服管理中心"
-			}, {
-				xtype : "button",
-				text : "业务管理中心"
-			}, {
-				xtype : "button",
-				text : "财务管理中心"
-			}, {
-				xtype : "button",
-				text : "信息技术部"
+				columnWidth : 0.3,
+				xtype : "mainright",
 			} ]
 		}, {
-			xtype : "panel",
-			title : "图片新闻",
-			items:[{
-				xtype:"image",
-				src:"resources/news/news1.jpg",
-				width:200,
-				height:200
-			},{
-				xtype:"button",
-				text:"1"
-			},{
-				xtype:"button",
-				text:"2"
-			},{
-				xtype:"button",
-				text:"3"
-			},{
-				xtype:"button",
-				text:"4"
-			}]
-		}, {
-			xtype : "tab",
-			title : "热点关注/理论学习/信息交流/网站维护"
-		}, {
-			xtype : "mainlogin",
-			title : "通知/发文"
-		}, {
-			xtype : "mainlogin",
-			title : "各部门信息"
-		}, {
-			xtype : "mainlogin",
-			title : "当前关注"
-		}, {
 			xtype : 'mainbottom',
-			height : 80
+			height : 100
 		} ]
+	},{
+		xtype:"panel",
+		flex:1
 	} ]
 });
