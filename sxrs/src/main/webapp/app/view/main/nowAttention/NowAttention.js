a4a8c03423b17bc9b2d8891ed21c3caa3936f20c
Administrator <Administrator@AX-wangjun-PC.AX.CLIC>
2014-12-10 16:25:56 +0800

    commit

--- sxrs/src/main/webapp/app/view/main/nowAttention/NowAttention.js
+++ sxrs/src/main/webapp/app/view/main/nowAttention/NowAttention.js
@@ -1,5 +1,5 @@
 /**
- * 中间的新闻
+ * 当前关注
  */
 Ext.define("app.view.main.nowAttention.NowAttention", {
 	extend : "Ext.panel.Panel",
@@ -20,27 +20,33 @@ Ext.define("app.view.main.nowAttention.NowAttention", {
 			} ],
 			data : [ {
 				src : 'resources/attentions/1.jpg',
-				caption : 'Drawing & Charts'
+				caption : 'Drawing & Charts',
+                url:"http://www.baidu.com"
 			}, {
 				src : 'resources/attentions/2.bmp',
-				caption : 'Advanced Data'
+				caption : 'Advanced Data',
+                url:"http://www.baidu1.com"
 			}, {
 				src : 'resources/attentions/3.jpg',
-				caption : 'Overhauled Theme'
+				caption : 'Overhauled Theme',
+                url:"http://www.baidu2.com"
 			}, {
 				src : 'resources/attentions/4.jpg',
-				caption : 'Performance Tuned'
+				caption : 'Performance Tuned',
+                url:"http://www.baidu3.com"
 			}, {
 				src : 'resources/attentions/2.bmp',
-				caption : 'Advanced Data'
+				caption : 'Advanced Data',
+                url:"http://www.baidu4.com"
 			}, {
 				src : 'resources/attentions/3.jpg',
-				caption : 'Overhauled Theme'
+				caption : 'Overhauled Theme',
+                url:"http://www.baidu5.com"
 			} ] 
 		}),
 		tpl : new Ext.XTemplate('<tpl for=".">',
 				'<div style="margin-bottom: 10px; float:left; margin-right: 10px; width:140px" class="view"> ',
-				'<center><img src="{src}" width="140" height="70"/>', '<br/><span><strong>{caption}</strong></span></center>', '</div>',
+				'<center><a href ="{url}" target = "blank"><img src="{src}" width="140" height="70"/>', '<br/><span><strong>{caption}</strong></span></a></center>', '</div>',
 				'</tpl>'),
 		itemSelector : 'div.view',
 		overItemCls : 'viewHover',
