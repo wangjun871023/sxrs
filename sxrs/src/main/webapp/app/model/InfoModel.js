c02d6e95db5416dfe97c7f36a28aff9eb1c4eec9
Administrator <Administrator@AX-wangjun-PC.AX.CLIC>
2014-12-30 11:30:04 +0800

    commit

--- sxrs/src/main/webapp/app/model/InfoModel.js
+++ sxrs/src/main/webapp/app/model/InfoModel.js
@@ -2,7 +2,9 @@ Ext.define('app.model.InfoModel', {
     extend: 'Ext.data.Model',
     fields: [
         {name: 'id',  type: 'string'},
-        {name: 'infoTitle',  type: 'string'},
+        {name: 'infoTitle',  type: 'string',convert:function(v,record){
+        	return "<a target='blank' href='info/showInfo.do?id="+record.data.id+"' title='"+v+"'>"+v+"</a>";
+        }},
         {name: 'infoTime',  type: 'string'},
         {name: 'infoCount',  type: 'string'},
         {name: 'infoTypeId',  type: 'string'}
